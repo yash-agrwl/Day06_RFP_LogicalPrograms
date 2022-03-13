36eb499f61b6b33d74bec9221bb7593505b9c0e3
yash-agrwl <agarwalyash7012@gmail.com>
2022-03-14 01:14:57 +0530

    added Problem 1: Fibonacci Series

---
+++ LogicalPrograms/FibonacciSeries.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class FibonacciSeries
+    {
+
+        static int Num1 = 0, Num2 = 1, InputNum;
+
+        public static void TakeInput()
+        {
+            Console.Write("Enter number of elements to print: ");
+            InputNum = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public static void CalculateNext()
+        {
+            int next;
+            for(int i = 3; i <= InputNum; i++)
+            {
+                next = Num1 + Num2;
+                Console.Write(next + " ");
+                Num1 = Num2;
+                Num2 = next;
+            }
+        }
+
+        public static void DisplayResult()
+        {
+            Console.Write(Num1 + " " + Num2 + " ");
+            CalculateNext();
+            Console.WriteLine();
+        }
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -7,6 +7,10 @@ namespace LogicalPrograms
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Computation Of Logical Problems\n");
+
+            Console.WriteLine("Problem 1: Fibonacci Series");
+            FibonacciSeries.TakeInput();
+            FibonacciSeries.DisplayResult();
         }
     }
 }
