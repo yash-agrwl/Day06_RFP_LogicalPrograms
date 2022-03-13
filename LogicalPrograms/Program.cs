4699825932401138ac43988654ae6a8399db12b8
yash-agrwl <agarwalyash7012@gmail.com>
2022-03-14 00:20:15 +0530

    added empty project file with welcome message

---
+++ LogicalPrograms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LogicalPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Computation Of Logical Problems\n");
+        }
+    }
+}
