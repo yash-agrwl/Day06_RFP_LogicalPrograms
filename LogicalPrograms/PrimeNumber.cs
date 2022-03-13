738e845bd309a4cd9dde2e880f47b19a2a08c87b
yash-agrwl <agarwalyash7012@gmail.com>
2022-03-14 01:55:11 +0530

    added Problem 3: Check if a number is Prime or not

---
+++ LogicalPrograms/PrimeNumber.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class PrimeNumber
+    {
+
+        static int InputNum;
+
+        public static void TakeInput()
+        {
+            Console.Write("Enter the Number: ");
+            InputNum = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public static bool Check()
+        {
+            int count = 0;
+            for (int i = 2; i <= InputNum / 2; i++)
+            {
+                if (InputNum % i == 0)
+                    count++;
+            }
+            if (count == 0 && InputNum != 1 && InputNum != 0)
+                return true;
+            else
+                return false;
+        }
+
+        public static void DisplayResult()
+        {
+            if (Check())
+                Console.WriteLine("Entered Number, " + InputNum + " is a Prime Number");
+            else
+                Console.WriteLine("Entered Number, " + InputNum + " is not a Prime Number");
+        }
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -15,6 +15,10 @@ namespace LogicalPrograms
             Console.WriteLine("\nProblem 2: Perfect Number");
             PerfectNumber.TakeInput();
             PerfectNumber.DisplayResult();
+
+            Console.WriteLine("\nProblem 3: Prime Number");
+            PrimeNumber.TakeInput();
+            PrimeNumber.DisplayResult();
         }
     }
 }
